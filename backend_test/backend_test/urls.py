d010bf1390209f2e75502b26b7ffb71aa0785cf0
Manuel E. Suarez Fardales <jlopezarencibia@Manus-MacBook-Air.local>
2022-02-12 01:03:21 -0600

    API Documentation

--- backend_test/backend_test/urls.py
+++ backend_test/backend_test/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
+schema_view = get_schema_view(title='Cornershop Test')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/menu/', include('menu.urls')),
     path('api/v1/menuuser/', include('menu_user.urls')),
     path('api/v1/user/', include('user.urls')),
+    path('schema/',schema_view)
 ]
